f44654007a91112bc095acb0ce0d67f2d684f821
Luis Barreto <luis.barreto.marin@gmail.com>
2021-09-02 10:33:22 -0400

    Pestaña de escuela

---
+++ first-aspnetcore/Controllers/EscuelaController.cs
@@ -0,0 +1,25 @@
+﻿using first_aspnetcore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_aspnetcore.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public ActionResult Index()
+        {
+            var escuela = new Escuela()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = "UCAB Guayana",
+                Fundacion = DateTime.Now,
+            };
+
+            return View(escuela);
+        }
+    }
+}
---
+++ first-aspnetcore/Models/Escuela.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_aspnetcore.Models
+{
+    public class Escuela
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fundacion { get; set; }
+    }
+}
